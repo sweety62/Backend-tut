28b37b4c3de511b301eac8908211f52a21fa3813
sweety62 <myselfsweety1@gmail.com>
2026-03-23 21:29:30 +0530

    understanding  about nodemon

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const http = require('http');
 http.createServer((req,resp)=>{
-  resp.write("<h1> hello this is sweety</h1>");
+  resp.write("<h1> hello this is sweety pursuing B.tech from cse department</h1>");
+
+  resp.end("hello! how are You!");
+
+}).listen(4800);
+
+
+
+http.createServer((req,resp)=>{
+  resp.write("<h2> Other servers</h2>");
 
   resp.end("hello!");
 
-}).listen(4800);
+}).listen(5800);
