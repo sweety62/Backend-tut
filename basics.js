ebe491b432e5462690e1f09adf2de2ba15729df0
sweety62 <myselfsweety1@gmail.com>
2026-03-23 19:45:32 +0530

    feat: start backend tutorial

---
+++ basics.js
@@ -0,0 +1,30 @@
+var  a =10;
+let b = 20;
+const c= 30;
+a =100;
+b = 200;
+//c =40;
+console.log(a+b+c);
+if(a==200){
+  console.log("this is if condition");
+}else{
+  console.log("this is else condition");
+}
+
+function fruit(item){
+  console.log("this is fruit "+item);
+}
+console.log('banana');
+
+//for loop
+for(var a =0;a<=10;a++){
+  console.log(a);
+}
+
+//while loop
+var a =0;
+while(a>=10){
+  
+  console.log(a);
+  a++;
+}
---
+++ first.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+
+fs.writeFileSync("sweety.txt","i am sweety" );
+
+
+/*console.log("Kg coding is the best.");
+
+console.log(20+20);
+
+function fruit(item){
+  console.log("fruit name is " + item);
+
+}
+fruit('apple');*/
+//var a =10;
+//var b = 20;
+//console.log(a+b);
