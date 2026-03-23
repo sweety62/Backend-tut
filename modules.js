7a8b662230b59b9bfa8f95da39399b9c421d2928
sweety62 <myselfsweety1@gmail.com>
2026-03-23 20:08:47 +0530

    learning about modules

---
+++ modules.js
@@ -0,0 +1,10 @@
+//const fs = require('fs');
+//const os = require('os');
+//fs.writeFileSync ("dummy.txt","trying with modules")
+const{log,warn} = require('console');
+//console.log(os.platform());
+//console.log(os.hostname());
+//console.log(os.cpus());
+console.log("abc")
+log("custom log")
+warn("custom log")
