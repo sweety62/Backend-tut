f44504ac7d2e49d9c1e19d007c9b3a9441ce9be3
sweety62 <myselfsweety1@gmail.com>
2026-03-23 21:06:06 +0530

    learning about external package

---
+++ colors.js
@@ -0,0 +1,6 @@
+const colors = require('colors');
+console.log(colors.red("Hii This is Sweety"));
+console.log(colors.green("Hii This is Sweety"));
+console.log(colors.yellow("Hii This is Sweety"));
+console.log(colors.yellow("Hii This is Sweety"));
+console.log(colors.green("Hii This is Sweety"));
