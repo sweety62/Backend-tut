5ab0cb2479996e06a07c3ceecc80c7d344828b25
sweety62 <myselfsweety1@gmail.com>
2026-03-24 19:41:57 +0530

    learned how to load HTML file using Nodes.js

---
+++ web.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const fs = require('fs');
+http.createServer((req,resp)=>{
+  fs.readFile('html/web.html','utf-8',(err,data)=>{
+    if(err) {
+      resp.writeHead(500,{"content-type":'text/plain'})
+      resp.write('internal server error');
+      resp.end();
+      return;
+    }
+    resp.writeHead(200,{"content-type":'text/html'});
+      resp.write(data);
+      resp.end();
+    
+
+  });
+
+}).listen(5500);
