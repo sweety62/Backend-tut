de3deb6e91bcf7233aeda97ebf41c64afbeb7767
sweety62 <myselfsweety1@gmail.com>
2026-03-31 19:30:41 +0530

    implement basic HTTP server with user form and data submission handling

---
+++ text/root.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const userForm = require('./userForm');
+const userDataSubmit = require('./userDataSubmit');
+http.createServer((req,resp)=>{
+  resp.writeHead(200,{"content-type":'text/html'})
+  if(req.url==="/"){
+    userForm(req,resp)
+  }else if (req.url==="/submit"){
+    userDataSubmit(req,resp);
+  }
+  resp.end();
+}).listen(2900)
---
+++ text/userDataSubmit.js
@@ -0,0 +1,9 @@
+function userDataSubmit(data, resp) {
+ 
+  resp.write(`
+    <h1>Received user data</h1>
+    
+  `);
+}
+
+module.exports = userDataSubmit;
